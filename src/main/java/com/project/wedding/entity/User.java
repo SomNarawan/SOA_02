bab893cded7ad32708ee0050bf62724321db2b14
Phutthichod <pin2017create@outlook.com>
2020-04-12 00:39:30 +0700

    pinto

---
+++ src/main/java/com/project/wedding/entity/User.java
@@ -0,0 +1,58 @@
+package com.project.wedding.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id_user;
+	private String title;
+	private String name;
+	private String surname;
+	private String phone;
+	private String email;
+	public Integer getId_user() {
+		return id_user;
+	}
+	public void setId_user(Integer id_user) {
+		this.id_user = id_user;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+
+}
